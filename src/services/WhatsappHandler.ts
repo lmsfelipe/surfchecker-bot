3686ccc9da742db33de9f9bc83089f5ccae05b7d
Felipe Lima <lms.felipe@gmail.com>
2025-04-21 17:21:28 -0300

    fix: call services in order to create surf forecast

--- src/services/WhatsappHandler.ts
+++ src/services/WhatsappHandler.ts
@@ -1,33 +1,57 @@
-import { Message } from 'whatsapp-web.js';
-import { ExtractedLocation, extractSurfSpot } from './surfSpotExtractor';
 import { serperAPI } from '../interfaces/serperApi';
 import { firecrawlAPI } from '../interfaces/firecrawlApi';
 import { surfForecastBuilder } from './surfForecastBuilder';
 
 export class WhatsappHandler {
-  async extractSurfSpotFromMessage(
-    message: Message,
-  ): Promise<ExtractedLocation | false> {
-    return extractSurfSpot(message.body);
+  async scrapeSpotUrls(
+    spotURLs: { domain: string; url: string; tagToScrape: string[] }[],
+  ): Promise<string> {
+    try {
+      const results = await Promise.all(
+        spotURLs.map(async (spot) => {
+          return firecrawlAPI(spot.url, spot.tagToScrape);
+        }),
+      );
+
+      console.log('scrapeSpotUrls ===>', results);
+      const scrapedUrls = results.flat().join(' ');
+
+      return scrapedUrls;
+    } catch (error) {
+      console.log('Error in scrapeSpotUrls:', error);
+      throw new Error(
+        'Scrape URLs: Não foi possível obter as informações do pico mencionado.',
+      );
+    }
   }
 
-  async getSurfForecast(message: Message): Promise<string> {
-    const surfSpot = await this.extractSurfSpotFromMessage(message);
+  async getSurfForecast(
+    message: string,
+    senderName: string | undefined,
+  ): Promise<string> {
+    try {
+      const spotURLs = await serperAPI(message);
+      console.log('spotURL ===>', spotURLs);
 
-    if (!surfSpot) {
-      return 'Não encontramos o pico mencionado na sua mensagem. Por favor, revise a mensagem e adicione o nome do pico e a cidade.';
-    }
+      const contentScraped = await this.scrapeSpotUrls(spotURLs);
+      console.log('contentScraped ===>', contentScraped);
 
-    const spotURL = await serperAPI(surfSpot);
-    const contentScraped = await firecrawlAPI(spotURL.data.organic[0].link);
+      const surfForecast = await surfForecastBuilder(
+        contentScraped,
+        senderName,
+      );
 
-    if (!contentScraped.markdown) {
+      console.log('!! Surf Forecast Completed !!');
+      return surfForecast;
+    } catch (error) {
+      console.log('Error in getSurfForecast:', error);
+
+      if (error instanceof Error) {
+        return error.message;
+      }
+
       return 'Não conseguimos encontrar as condições do mar para o pico mencionado. Tente novamente mais tarde.';
     }
-
-    const surfForecast = await surfForecastBuilder(contentScraped.markdown);
-
-    return surfForecast;
   }
 
   // Gather user and response infos from IA and whatsapp message
