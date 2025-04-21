fe3c8b3795fd410c6b398764a623196ce0a56a21
Felipe Lima <lms.felipe@gmail.com>
2025-04-21 17:16:46 -0300

    fix: firecrawl adjustments

--- src/interfaces/firecrawlApi.ts
+++ src/interfaces/firecrawlApi.ts
@@ -1,45 +1,22 @@
-import FirecrawlApp, {
-  CrawlParams,
-  CrawlStatusResponse,
-} from '@mendable/firecrawl-js';
+import FirecrawlApp, { ScrapeResponse } from '@mendable/firecrawl-js';
 
 const app = new FirecrawlApp({ apiKey: process.env.FIRECRAWL_API_KEY });
 
 // Scrape a website
-export async function firecrawlAPI(url: string) {
+export async function firecrawlAPI(
+  url: string,
+  includeTags: string[],
+): Promise<string> {
   console.log('start scrape', url);
 
   const scrapeResponse = await app.scrapeUrl(url, {
     formats: ['markdown'],
+    includeTags,
   });
 
-  if (!scrapeResponse.success) {
-    throw new Error(`Failed to scrape: ${scrapeResponse.error}`);
+  if (!scrapeResponse.success || !scrapeResponse.markdown) {
+    return '';
   }
 
-  return scrapeResponse;
+  return scrapeResponse.markdown;
 }
-
-// import axios from 'axios';
-
-// // TODO: Refactor this function to isolate the API call and make it reusable
-// export async function firecrawlAPI(url: string): Promise<> {
-//   const data = {
-//     url,
-//     formats: ['markdown'],
-//     mobile: true,
-//   };
-
-//   const config = {
-//     method: 'post',
-//     maxBodyLength: Infinity,
-//     url: 'https://api.firecrawl.dev/v1/scrape',
-//     headers: {
-//       Authorization: `Bearer ${process.env.FIRECRAWL_API_KEY}`,
-//       'Content-Type': 'application/json',
-//     },
-//     data,
-//   };
-
-//   return axios.request(config);
-// }
