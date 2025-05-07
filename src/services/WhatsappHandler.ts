900ee93a70cdc6175c1e21323e7f7d83c98f4085
Felipe Lima <lms.felipe@gmail.com>
2025-05-07 16:50:55 -0300

    fix: removes multiple url scrapes

--- src/services/WhatsappHandler.ts
+++ src/services/WhatsappHandler.ts
@@ -3,42 +3,26 @@ import { firecrawlAPI } from '../interfaces/firecrawlApi';
 import { surfForecastBuilder } from './surfForecastBuilder';
 
 export class WhatsappHandler {
-  async scrapeSpotUrls(
-    spotURLs: { domain: string; url: string; tagToScrape: string[] }[],
-  ): Promise<string> {
-    try {
-      const results = await Promise.all(
-        spotURLs.map(async (spot) => {
-          return firecrawlAPI(spot.url, spot.tagToScrape);
-        }),
-      );
-
-      console.log('scrapeSpotUrls ===>', results);
-      const scrapedUrls = results.flat().join(' ');
-
-      return scrapedUrls;
-    } catch (error) {
-      console.log('Error in scrapeSpotUrls:', error);
-      throw new Error(
-        'Scrape URLs: Não foi possível obter as informações do pico mencionado.',
-      );
-    }
-  }
-
   async getSurfForecast(
     message: string,
     senderName: string | undefined,
   ): Promise<string> {
     try {
-      const spotURLs = await serperAPI(message);
-      console.log('spotURL ===>', spotURLs);
+      console.log('ws message ===>', message);
 
-      const contentScraped = await this.scrapeSpotUrls(spotURLs);
+      const spotURL = await serperAPI(message);
+      console.log('spotURL ===>', spotURL);
+
+      const contentScraped = await firecrawlAPI(
+        spotURL.url,
+        spotURL.tagToScrape,
+      );
       console.log('contentScraped ===>', contentScraped);
 
       const surfForecast = await surfForecastBuilder(
         contentScraped,
         senderName,
+        message,
       );
 
       console.log('!! Surf Forecast Completed !!');
