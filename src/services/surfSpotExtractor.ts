b1dc77f5bf8d6891b073cdac1eea645a7a7dfc30
Felipe Lima <lms.felipe@gmail.com>
2025-05-15 15:38:20 -0300

    fix: surf spot message extraction adjustments

--- src/services/surfSpotExtractor.ts
+++ src/services/surfSpotExtractor.ts
@@ -3,10 +3,11 @@ import { openAiAPI, GPTModel } from '../interfaces/openAiAPI';
 export interface ExtractedLocation {
   spot: string;
   city: string;
+  state: string;
 }
 
 const throwErrorMessage = () => {
-  console.error('Error: No spot or city found');
+  console.error('Erro: Nenhum pico ou cidade encontrado');
 
   throw new Error(
     'Não encontramos o pico mencionado na sua mensagem. Por favor, revise a mensagem e adicione o nome do pico e a cidade.',
@@ -15,14 +16,18 @@ const throwErrorMessage = () => {
 
 export async function extractSurfSpot(
   userMessage: string,
-): Promise<ExtractedLocation> {
+): Promise<ExtractedLocation | false> {
   try {
-    const response = await openAiAPI(GPTModel.GPT4o_MINI, [
+    const response = await openAiAPI(GPTModel.GPT4o, [
       {
         role: 'system',
-        content: `Your are a bot assistant that only extracts a surf spot and city from a user message.
-                  The format you should return is "SpotName, City". You also need to verify if the city actually is on the coast.
-                  If no spot name is found or city isn't on the coast, respond with "false".`,
+        content: `Você é um assistente que extrai o pico de surf, cidade e estado de uma mensagem do usuário.`,
+      },
+      {
+        role: 'user',
+        content: `O formato que você deve retornar é "pico, cidade, estado".
+                  Se não encontrar pico, cidade ou estado, responda com string false.
+                  Se você não conseguir encontrar nenhum pico, cidade ou estado, tente pelo menos extrair o pico de surf da mensagem.`,
       },
       {
         role: 'user',
@@ -32,17 +37,23 @@ export async function extractSurfSpot(
 
     const extractedText = response.choices[0]?.message?.content;
 
+    console.log('extractedText ===>', extractedText);
+
     if (!extractedText) {
       return throwErrorMessage();
     }
 
-    const [spot, city] = extractedText.split(',').map((s) => s.trim());
+    if (extractedText === 'false') {
+      return false;
+    }
 
-    if (!spot || !city) {
+    const [spot, city, state] = extractedText.split(',').map((s) => s.trim());
+
+    if (!spot && !city && !state) {
       return throwErrorMessage();
     }
 
-    return { spot, city };
+    return { spot, city, state };
   } catch (error) {
     return throwErrorMessage();
   }
