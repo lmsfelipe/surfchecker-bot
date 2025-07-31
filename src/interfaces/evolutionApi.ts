b6869d2e764a4a980e4bfacdf46be400fe0fd4ae
Felipe Lima <lms.felipe@gmail.com>
2025-07-31 17:17:52 -0300

    fix: change instance name in evolution send message request

--- src/interfaces/evolutionApi.ts
+++ src/interfaces/evolutionApi.ts
@@ -7,12 +7,13 @@ interface SendMessageRequest {
 
 const EVOLUTION_API_KEY = process.env.EVOLUTION_API_KEY;
 const EVOLUTION_API_URL = process.env.EVOLUTION_API_URL;
+const EVOLUTION_INSTANCE_NAME = process.env.EVOLUTION_INSTANCE_NAME || 'zeeky';
 
 export const evolutionApiService = {
   async sendTextMessage(data: SendMessageRequest): Promise<any> {
     try {
       const response = await request.post(
-        `${EVOLUTION_API_URL}/message/sendText/checker`,
+        `${EVOLUTION_API_URL}/message/sendText/${EVOLUTION_INSTANCE_NAME}`,
         data,
         {
           headers: {
